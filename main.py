cbf669691111956f36d785b32f24163939a37f3f
Solid Quant <solidquant@gmail.com>
2023-06-27 21:53:52 +0900

    adding contract call benchmarks

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ def benchmark_tx_requests(w3: Web3, block_number: int):
         # print(f'{i}/{block_number}', b)
 
 
+def benchmark_contract_call(w3: Web3):
+    # USDC/ETH 0.05% pool
+    uniswap_v3_pool_address = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640'  # checksum address
+    uniswap_v3_pool_abi = [
+        {
+            'inputs': [],
+            'name': 'factory',
+            'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}],
+            'stateMutability': 'view',
+            'type': 'function'
+        }
+    ]
+    pool = w3.eth.contract(address=uniswap_v3_pool_address, abi=uniswap_v3_pool_abi)
+    factory_address = pool.functions.factory().call()
+    # print(factory_address)
+
+
 if __name__ == '__main__':
     free_w3 = Web3(Web3.HTTPProvider(ALCHEMY_FREE_RPC_URL))
     paid_w3 = Web3(Web3.HTTPProvider(ALCHEMY_PAID_RPC_URL))
@@ -23,11 +40,22 @@ if __name__ == '__main__':
 
     block_number = free_w3.eth.get_block('latest').number
 
-    t1 = timeit.timeit(lambda: benchmark_tx_requests(free_w3, block_number), number=1)
-    print(f'Free tier: {t1} seconds')  # 26.88 seconds
+    # TX requests
+    # t1 = timeit.timeit(lambda: benchmark_tx_requests(free_w3, block_number), number=1)
+    # print(f'Free tier: {t1} seconds')  # 26.88 seconds
+    #
+    # t2 = timeit.timeit(lambda: benchmark_tx_requests(paid_w3, block_number), number=1)
+    # print(f'Paid tier: {t2} seconds')  # 28 seconds
+    #
+    # t3 = timeit.timeit(lambda: benchmark_tx_requests(local_w3, block_number), number=1)
+    # print(f'Local: {t3} seconds')  # 0.54 seconds
 
-    t2 = timeit.timeit(lambda: benchmark_tx_requests(paid_w3, block_number), number=1)
-    print(f'Paid tier: {t2} seconds')  # 28 seconds
+    # Contract calls
+    t1 = timeit.timeit(lambda: benchmark_contract_call(free_w3), number=10)
+    print(f'Free tier: {t1 / 10.0} seconds')
 
-    t3 = timeit.timeit(lambda: benchmark_tx_requests(local_w3, block_number), number=1)
-    print(f'Local: {t3} seconds')
+    t2 = timeit.timeit(lambda: benchmark_contract_call(paid_w3), number=10)
+    print(f'Paid tier: {t2 / 10.0} seconds')
+
+    t3 = timeit.timeit(lambda: benchmark_contract_call(local_w3), number=10)
+    print(f'Local: {t3 / 10.0} seconds')
